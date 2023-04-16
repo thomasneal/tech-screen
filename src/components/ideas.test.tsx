5b5f55bb370af7c167f23966aa9565fe34baee5e
Dan Page <dan.page91@gmail.com>
2023-04-16 16:11:00 +0100

    dan call

--- src/components/ideas.test.tsx
+++ src/components/ideas.test.tsx
@@ -1,28 +1,64 @@
-import { render, screen, fireEvent } from '@testing-library/react'
-import Ideas from './ideas'
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Ideas from "./ideas";
 
-describe('Ideas', () => {
-  it('has New Idea button', () => {
-    render(<Ideas />)
+describe("Ideas", () => {
+  it("has New Idea button", () => {
+    render(<Ideas />);
 
-    const button = screen.getByRole('button', {
+    const button = screen.getByRole("button", {
       name: /New Idea/i,
-    })
+    });
 
     expect(button).toBeInTheDocument();
-  })
+  });
 
-  it('creating a new Idea adds a new item to Ideas', () => {
-   const { container }  = render(<Ideas />)
+  it("creating a new Idea adds a new item to Ideas", () => {
+    const { container } = render(<Ideas />);
 
-    const button = screen.getByRole('button', {
+    const button = screen.getByRole("button", {
       name: /New Idea/i,
-    })
+    });
 
-    fireEvent.click(button);
+    fireEvent.click(button); // use userEvent
 
-    console.log(container);
+    // console.log(container);
 
     expect(button).toBeInTheDocument();
-  })
-})
+  });
+
+  it.only("should delete an idea", async () => {
+    // 1 - render the ideas
+    const user = userEvent.setup();
+    render(<Ideas />);
+
+    const button = screen.getByRole("button", {
+      name: /New Idea/i,
+    });
+
+    expect(
+      screen.queryByRole("button", {
+        name: /Delete/i,
+      })
+    ).not.toBeInTheDocument();
+
+    await user.click(button);
+
+    const deleteButton = screen.getByRole("button", {
+      name: /Delete/i,
+    });
+
+    // 3 - expect there should be one idea
+    expect(deleteButton).toBeInTheDocument();
+
+    // 3 - I'm going delete the one
+    await user.click(deleteButton);
+
+    // 4 - expect there are no more ideas
+    expect(
+      screen.queryByRole("button", {
+        name: /Delete/i,
+      })
+    ).not.toBeInTheDocument();
+  });
+});
--- src/components/ideas.tsx
+++ src/components/ideas.tsx
@@ -1,73 +1,74 @@
 import { useEffect, useReducer } from "react";
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { Idea } from "@/types/Idea";
 import IdeaCard from "./idea";
 import styles from "@/styles/Ideas.module.scss";
 
-
 type SortOptions = "alpha" | "updated";
 
 type State = {
-  ideasLoaded: boolean,
-  newIdea: Idea,
-  sort: SortOptions,
-  ideas: Idea[]
-}
+  ideasLoaded: boolean;
+  newIdea: Idea;
+  sort: SortOptions;
+  ideas: Idea[];
+};
 
 type CreateAction = {
-  type: "CREATE_IDEA"
+  type: "CREATE_IDEA";
 };
 
 type HydrateAction = {
-  type: "HYDRATE_IDEAS",
+  type: "HYDRATE_IDEAS";
   ideas: Idea[];
 };
 
 type UpdateAction = {
-  type: "UPDATE_IDEA",
-  id: string,
-  title: string,
-  description: string,
+  type: "UPDATE_IDEA";
+  id: string;
+  title: string;
+  description: string;
 };
 
 type DestroyAction = {
-  type: "DELETE_IDEA",
-  id: string
+  type: "DELETE_IDEA";
+  id: string;
 };
 
 type SortAction = {
-  type: "SORT_IDEAS",
-  sort: SortOptions
+  type: "SORT_IDEAS";
+  sort: SortOptions;
 };
 
-type Actions = 
-  CreateAction 
+type Actions =
+  | CreateAction
   | HydrateAction
   | UpdateAction
   | DestroyAction
   | SortAction;
 
-const generateNewIdea = (): Idea => ({
-  id: uuidv4(),
-  title: "",
-  description: "description",
-  lastUpdated: new Date().toString(),
-});
+const generateNewIdea = (): Idea => {
+  return {
+    id: uuidv4(),
+    title: "",
+    description: "description",
+    lastUpdated: new Date().toString(),
+  };
+};
 
 const initialState = {
   ideasLoaded: false,
   newIdea: generateNewIdea(),
   sort: "updated" as const,
-  ideas: []
-}
+  ideas: [],
+};
 
 function reducer(state: State, action: Actions) {
   switch (action.type) {
-    case 'CREATE_IDEA': {
+    case "CREATE_IDEA": {
       return {
-       ...state,
+        ...state,
         ideas: [...state.ideas, state.newIdea],
-        newIdea: generateNewIdea()
+        newIdea: generateNewIdea(),
       };
     }
     case "HYDRATE_IDEAS": {
@@ -79,42 +80,42 @@ function reducer(state: State, action: Actions) {
     }
     case "UPDATE_IDEA": {
       const updatedIdeas = [...state.ideas];
-      const ideaToUpdate = updatedIdeas.find(
-        (idea) => idea.id === action.id
-      )
+      const ideaToUpdate = updatedIdeas.find((idea) => idea.id === action.id);
 
       if (ideaToUpdate) {
         ideaToUpdate.title = action.title;
         ideaToUpdate.description = action.description;
         ideaToUpdate.lastUpdated = new Date().toString();
-      }  
+      }
       return {
         ...state,
-        ideas: updatedIdeas
+        ideas: updatedIdeas,
       };
     }
     case "DELETE_IDEA": {
       return {
         ...state,
-        ideas: state.ideas.filter((idea) => idea.id !== action.id)
-      }
+        ideas: state.ideas.filter((idea) => idea.id !== action.id),
+      };
     }
     case "SORT_IDEAS": {
       let sortedIdeas = state.ideas;
 
-      if (action.sort === 'alpha') {
-        sortedIdeas = state.ideas.sort((a, b) => (a.title > b.title) ? 1 : -1);
+      if (action.sort === "alpha") {
+        sortedIdeas = state.ideas.sort((a, b) => (a.title > b.title ? 1 : -1));
       }
-      
-      if (action.sort === 'updated') {
-        sortedIdeas = state.ideas.sort((a, b) => (new Date(a.lastUpdated) < new Date(b.lastUpdated)) ? 1 : -1);     
+
+      if (action.sort === "updated") {
+        sortedIdeas = state.ideas.sort((a, b) =>
+          new Date(a.lastUpdated) < new Date(b.lastUpdated) ? 1 : -1
+        );
       }
 
       return {
         ...state,
         sort: action.sort,
-        ideas: sortedIdeas
-      }
+        ideas: sortedIdeas,
+      };
     }
     default:
       return state;
@@ -128,7 +129,7 @@ export default function Ideas() {
     const stringToParse = localStorage.getItem("ideas");
     if (stringToParse) {
       const itemsArray = JSON.parse(stringToParse);
-      dispatch({ type: "HYDRATE_IDEAS", ideas: itemsArray})
+      dispatch({ type: "HYDRATE_IDEAS", ideas: itemsArray });
     }
   }, []);
 
@@ -136,36 +137,45 @@ export default function Ideas() {
     if (state.ideasLoaded || state.ideas.length > 0) {
       localStorage.setItem("ideas", JSON.stringify(state.ideas));
     }
-    
   }, [state.ideas, state.ideasLoaded]);
-  
+
   const handleNewIdea = () => dispatch({ type: "CREATE_IDEA" });
 
   const handleDelete = (id: string) => dispatch({ type: "DELETE_IDEA", id });
 
   const handleUpdate = (id: string, title: string, description: string) => {
-    dispatch({ type: "UPDATE_IDEA", id, title, description })
+    dispatch({ type: "UPDATE_IDEA", id, title, description });
   };
 
-  const handleSort = (sort: SortOptions) => dispatch({ type: "SORT_IDEAS", sort});
+  const handleSort = (sort: SortOptions) =>
+    dispatch({ type: "SORT_IDEAS", sort });
 
   return (
     <section>
-        <label className={styles.sortLabel}>Sort</label>
-        <select className={styles.sortSelect}
-          id="sort"
-          value={state.sort}
-          onChange={(e) => handleSort(e.target.value as SortOptions)}
-        >
-          <option value={"updated"}>Last Updated</option>
-          <option value={"alpha"}>Alphabetically</option>
-        </select>
+      <label className={styles.sortLabel}>Sort</label>
+      <select
+        className={styles.sortSelect}
+        id="sort"
+        value={state.sort}
+        onChange={(e) => handleSort(e.target.value as SortOptions)}
+      >
+        <option value={"updated"}>Last Updated</option>
+        <option value={"alpha"}>Alphabetically</option>
+      </select>
       <section className={styles.ideasContainer}>
-        {state.ideas && state.ideas.map((idea) => (
-          <IdeaCard key={idea.id} idea={idea} handleDelete={handleDelete} handleUpdate={handleUpdate} />
-        ))}
+        {state.ideas &&
+          state.ideas.map((idea) => (
+            <IdeaCard
+              key={idea.id}
+              idea={idea}
+              handleDelete={handleDelete}
+              handleUpdate={handleUpdate}
+            />
+          ))}
       </section>
-      <button className={styles.primary} onClick={handleNewIdea}>New Idea</button>
+      <button className={styles.primary} onClick={handleNewIdea}>
+        New Idea
+      </button>
     </section>
   );
 }
