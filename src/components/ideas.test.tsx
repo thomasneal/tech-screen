2216daccc85cdc46cb74598cf36a4073d92fcd8d
Tom Neal <thomasevanneal@gmail.com>
2023-04-16 06:46:40 -0500

    WIP: Ideas tests

--- src/components/ideas.test.tsx
+++ src/components/ideas.test.tsx
@@ -1,13 +1,27 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Ideas from './ideas'
 
 describe('Ideas', () => {
   it('has New Idea button', () => {
-    const { container } = render(<Ideas />)
+    render(<Ideas />)
 
     const button = screen.getByRole('button', {
       name: /New Idea/i,
     })
+
+    expect(button).toBeInTheDocument();
+  })
+
+  it('creating a new Idea adds a new item to Ideas', () => {
+   const { container }  = render(<Ideas />)
+
+    const button = screen.getByRole('button', {
+      name: /New Idea/i,
+    })
+
+    fireEvent.click(button);
+
+    console.log(container);
 
     expect(button).toBeInTheDocument();
   })
